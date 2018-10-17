e07b0d10e7fc15e1a67aeb8e6e161532c9b6dc54
HomeACcessoryKid <homeaccessorykid@gmail.com>
2018-10-17 08:46:56 +0200

    initial parse attempt
    not yet validated

--- uart_config.c
+++ uart_config.c
@@ -40,11 +40,37 @@ void uart_send_data(void *pvParameters){
     }
 }
 
-void uart_print_input(void *pvParameters) {
+int buff[17]; //longest message + 1
+int idx=0;
+void uart_parse_input(void *pvParameters) {
     char ch;
     for(;;) {
-        ch=(char)uart_getc(0);
-        uart_putc(1,ch);
+        buff[idx++]=uart_getc(0);
+        while (idx){
+            if (!(buff[0]==0x8c || buff[0]==0x55)) {          shift_buff(1); continue;}
+            if   (buff[0]==0x8c && buff[1]==0xfc)  {parse(2); shift_buff(2); continue;}
+            if   (buff[0]==0x8c && idx>=2)         {          shift_buff(1); continue;}
+            if (  buff[0]==0x55 && !(buff[1]==0xfe && buff[2]==0xfe) && idx>2 ) {
+                                                              shift_buff(1); continue;} //now for sure 0x55fefe
+            if (  buff[3]==0 || buff[3]>4 && idx>3){          shift_buff(1); continue;}
+            if (  buff[4]==0 || buff[4]>9 && idx>4){          shift_buff(1); continue;}
+            if (  buff[3]==3 && idx>=7 )  {
+                if (crc(7))  {parse(7); shift_buff(7); continue;}
+                if (crc(8))  {parse(8); shift_buff(8); continue;}
+                if (idx>=8)  {          shift_buff(7); continue;} // failure for type 3 so flush it
+            }
+            if (  (buff[3]==1||buff[3]==2) && idx>=8 ) {
+                if (crc(8))  {parse(8); shift_buff(8); continue;}
+                if (crc(9))  {parse(9); shift_buff(9); continue;}
+                if (idx>=9)  {          shift_buff(8); continue;} // failure for type 1 or 2 so flush it
+            }
+            if ( buff[3]==4 && idx==16)  {
+                if (crc(16))  parse(16);
+                shift_buff(16); continue;
+            }
+            if (idx==16) printf("something went wrong: idx=16!\n");
+            break;
+        }
     }
 }
 
@@ -97,15 +123,14 @@ void uart_print_config(void *pvParameters){
 }
 
 void user_init(void){
+    /* Activate UART for GPIO2 */
+    gpio_set_iomux_function(2, IOMUX_GPIO2_FUNC_UART1_TXD);
+    uart_set_baud(1, 9600);
     uart_set_baud(0, 9600);
+    
     printf("SDK version:%s\n", sdk_system_get_sdk_version());
     
 //    xTaskCreate(uart_send_data, "tsk1", 256, NULL, 2, NULL);
-    /* Activate UART for GPIO2 */
-    gpio_set_iomux_function(2, IOMUX_GPIO2_FUNC_UART1_TXD);
-    
-    /* Set baud rate of UART1 to 100 (so it's easier to measure) */
-    uart_set_baud(1, 9600);
 
-    xTaskCreate(uart_print_input, "tsk2", 256, NULL, 1, NULL);
+    xTaskCreate(uart_parse_input, "parse", 256, NULL, 1, NULL);
 }
